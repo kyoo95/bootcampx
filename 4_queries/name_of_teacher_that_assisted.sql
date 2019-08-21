e333f3220afb0439b4cd323ee12d908546028d08
Kevin <kevinyoo0203@yahoo.com>
2019-08-21 23:08:36 +0000

    parameterized queries

--- 4_queries/name_of_teacher_that_assisted.sql
+++ 4_queries/name_of_teacher_that_assisted.sql
@@ -3,5 +3,5 @@ FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = 'JUL02'
+WHERE cohorts.name LIKE 'JUL02'
 ORDER BY teachers.name;
